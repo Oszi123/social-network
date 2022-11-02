73d358978cb67472019623b9c808884634e41e23
Oszi123 <oscar.bonilla123@outlook.com>
2022-11-02 11:29:24 -0700

    Models were created and connected and also refactored module files

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,18 +10,17 @@ const {
     deleteFriend
 } = require('../../controllers/user-controller');
 
-// /api/users
+
 router.route('/')
 .get(getUsers)
 .post(addUser);
 
-// /api/users/:id
+
 router.route('/:id')
 .get(getUserByID)
 .put(updateUser)
 .delete(deleteUser);
 
-// /api/users/:id/friends/:friendId
 
 router.route('/:id/friends/:friendID')
 .post(addFriend)
